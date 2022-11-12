62a722c0b25c761b8326675aa14da17fb4136410
gustavoss-cit <gustavoss@ciandt.com>
2022-11-12 16:58:34 -0300

    Initial commit

---
+++ src/main/java/br/com/gusttaleal/webapi/JavaWebApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gusttaleal.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gusttaleal/webapi/JavaWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gusttaleal.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
